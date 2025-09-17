a7cc43840483db952351d3a475c95fe08f4c02cc
Gabriel Sandes <165197356+Gabrielsande@users.noreply.github.com>
2025-09-17 17:56:22 -0300

    primeiro commit

---
+++ src/br/ulbra/DAO/ClienteDAO.java
@@ -0,0 +1,99 @@
+package br.ulbra.DAO;
+
+import static br.ulbra.DAO.AbstractDAO.getConnection;
+import br.ulbra.model.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteDAO extends AbstractDAO implements CrudRepository<Cliente> {
+
+//Salvar
+    @Override
+    public void salvar(Cliente c) throws SQLException {
+        String sql = "INSERT INTO cliente (nome, email, telefone) VALUES (?, ?, ?)";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, c.getNome());
+            ps.setString(2, c.getEmail());
+            ps.setString(3, c.getTelefone());
+            int affected = ps.executeUpdate();
+            if (affected == 0) {
+                throw new SQLException("Falha ao inserir cliente.");
+            }
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    c.setId(rs.getInt(1));
+                }
+            }
+        }
+    }
+//Buscar
+    @Override
+    public Cliente buscarPorId(int id) throws SQLException {
+        String sql = "SELECT id, nome, email, telefone FROM cliente WHERE id = ?";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new Cliente(
+                            rs.getInt("id"),
+                            rs.getString("nome"),
+                            rs.getString("email"),
+                            rs.getString("telefone")
+                    );
+                }
+            }
+        }
+        return null;
+
+    }
+//Listagem
+    @Override
+    public List<Cliente> listar() throws SQLException {
+        String sql = "SELECT id, nome, email, telefone FROM cliente ORDER by id";
+        List<Cliente> lista = new ArrayList<>();
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                Cliente c = new Cliente(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getString("email"),
+                        rs.getString("telefone")
+                );
+                lista.add(c);
+            }
+        }
+        return lista;
+    }
+//Atualizar
+    @Override
+    public void atualizar(Cliente c) throws SQLException {
+        String sql = "UPDATE cliente set nome = ?, email = ?, telefone = ? where id = ?";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, c.getNome());
+            ps.setString(2, c.getEmail());
+            ps.setString(3, c.getTelefone());
+            ps.setInt(4, c.getId());
+            ps.executeUpdate();
+        }
+    }
+//Excluir 
+    public void remover(int id) throws SQLException {
+        String sql = "DELETE FROM Cliente where id = ?";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        }
+    }
+
+}
